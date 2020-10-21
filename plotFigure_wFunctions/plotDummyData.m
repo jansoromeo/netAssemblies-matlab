c24f77a3d4987b159116dd3d114899878be2f7aa
roeegilron <roeegilron@gmail.com>
2020-10-21 15:15:02 -0700

    add check to see if hax axists for axis handle

--- plotFigure_wFunctions/plotDummyData.m
+++ plotFigure_wFunctions/plotDummyData.m
@@ -1,8 +1,10 @@
-function plotDummyData(varargin)
+function plotDummyData()
 hfig = findall(0,'Type','figure','tag','FigureStateSpace'); 
 hax = findall(hfig,'Type','Axes','tag','StateSpaceAxes'); 
-cla(hax);
-xvals = randperm(10,10);
-yvals = randperm(10,10);
-PlotData(xvals,yvals);
+if ~isempty(hax)
+    cla(hax);
+    xvals = randperm(10,10);
+    yvals = randperm(10,10);
+    PlotData(xvals,yvals);
+end
 end
